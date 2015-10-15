bd4d50401e14d6d2b770125557fdb7114b7948d8
David Boster <David@Boster-iMac.local>
2015-10-15 06:51:54 -0500

    Initial commit

---
+++ teapot-template/typings/tsd.d.ts
@@ -0,0 +1,8 @@
+
+/// <reference path="angularfire/angularfire.d.ts" />
+/// <reference path="angularjs/angular-resource.d.ts" />
+/// <reference path="angularjs/angular-route.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="firebase/firebase.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="requirejs/require.d.ts" />
